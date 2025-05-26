e9567640dd2adbab045d7e98f88af374d770adaf
afaan-02 <afaanmomin2@gmail.com>
2025-05-26 08:48:36 +0530

    Delete Network Security/Netwrok-Traffic-Analyzer /scripts directory

--- Network Security/Netwrok-Traffic-Analyzer /scripts/analyze.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def analyze_traffic():
-    df = pd.read_csv("data/traffic_data.csv")
-    print("Traffic Summary:")
-    print(df.describe())
-
-if __name__ == "__main__":
-    analyze_traffic()
